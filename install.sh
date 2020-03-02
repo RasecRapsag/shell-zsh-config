0061b1c30b2fe82641df83f0c9d23ad352f04124
Cesar Gaspar <rasec.rapsag@gmail.com>
2020-03-02 13:04:50 -0300

    Adicionado configuração do Bash

--- install.sh
+++ install.sh
@@ -13,4 +13,11 @@ fi
 cp -v files/profile ~/${DIR}/
 ln -sv ~/${DIR}/profile ~/.profile
 
+## Bash config
+echo -e "\nBash config"
+if [ -f ~/.bashrc ] || [ -L ~/.bashrc ]; then
+    rm -v ~/.bashrc
+fi
 
+cp -rfv files/bash ~/${DIR}/
+ln -sv ~/${DIR}/bash/bashrc ~/.bashrc
