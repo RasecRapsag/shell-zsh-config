57760b388f8c70572027a0dca85e3dda6730395a
Cesar Gaspar <rasec.rapsag@gmail.com>
2020-03-21 21:18:30 -0300

    Adicionado o asdf no zsh

--- install.sh
+++ install.sh
@@ -31,7 +31,6 @@ fi
 cp -rfv files/vim ~/${DIR}/
 ln -sv ~/${DIR}/vim/vimrc ~/.vimrc
 
-
 ## Tmux config
 echo -e "\nTmux config"
 if [ -f ~/.tmux.conf ] || [ -L ~/.tmux.conf ]; then
@@ -40,7 +39,6 @@ fi
 
 cp -rfv files/tmux ~/${DIR}/
 ln -sv ~/${DIR}/tmux/tmux.conf ~/.tmux.conf
-
 
 ## Zsh config
 echo -e "\nZsh config"
@@ -74,6 +72,21 @@ cp -rfv files/zsh ~/${DIR}/
 ln -sv ~/${DIR}/zsh/zshrc ~/.zshrc
 ln -s ~/${DIR}/zsh/aws_zsh_completer.sh ~/${DIR}/bin
 
-clear
-source ~/.zshrc
-exit
+## ASDF
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.7.7
+. ~/.asdf/asdf.sh
+
+## ASDF Plugin
+asdf plugin-add python
+asdf plugin-add ruby https://github.com/asdf-vm/asdf-ruby.git
+asdf plugin-add nodejs https://github.com/asdf-vm/asdf-nodejs.git
+bash ~/.asdf/plugins/nodejs/bin/import-release-team-keyring
+asdf plugin-add golang https://github.com/kennyp/asdf-golang.git
+asdf plugin-add erlang https://github.com/asdf-vm/asdf-erlang.git
+asdf plugin-add elixir https://github.com/asdf-vm/asdf-elixir.git
+asdf plugin-add kotlin https://github.com/missingcharacter/asdf-kotlin.git
+asdf plugin-add rust https://github.com/code-lever/asdf-rust.git
+asdf plugin-add crystal https://github.com/marciogm/asdf-crystal.git
+
+## load ZSH
+zsh
